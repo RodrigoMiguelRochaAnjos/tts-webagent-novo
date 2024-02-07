195d1c87a38fc62b0155fba48e5b89ac6880d7c2
Miguel Almada <miguel.almada@tts.com>
2024-02-07 18:13:38 -0100

    WEBAGENT-258 add and fix translations

--- src/app/modules/neo/features/search/ui/tab-price-details/module/tab-price-details.module.ts
+++ src/app/modules/neo/features/search/ui/tab-price-details/module/tab-price-details.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { TabPriceDetailsComponent } from "../tab-price-details.component";
 import { CommonModule } from "@angular/common";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
     declarations: [TabPriceDetailsComponent],
     imports: [
         CommonModule,
+        SharedModule
     ],
     exports: [
         TabPriceDetailsComponent
